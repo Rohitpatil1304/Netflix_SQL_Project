ea779d8df95228955f5291c320b1a3ec13c0143a
Rohit Patil <144587244+Rohitpatil1304@users.noreply.github.com>
2025-02-13 20:30:22 +0530

    Update and rename SQL_Project.sql to Solution_Project.sql

--- Solution_Project.sql
+++ Solution_Project.sql
@@ -1,24 +1,5 @@
 -- Netflix Project --
 
-
-CREATE TABLE  Netflix(
-	
-	show_id	VARCHAR(10) ,
-	type VARCHAR (10) ,
-	title VARCHAR(150) ,	
-	director VARCHAR(208) ,
-	casts VARCHAR(1000) ,
-	country	VARCHAR(150) ,
-	date_added VARCHAR(50),
-	release_year INT ,
-	rating	VARCHAR(10),
-	duration VARCHAR (15),
-	listed_in VARCHAR(100),
-	description VARCHAR(250)
-
-);
----------------------------------------------------------------------------
-
 --1] COUNT the number of TV shows and Movies ;
 
 select 
